af4020296d03ac02fededda90fd7839c42ac717c
salama <a541996salamh@gmail.com>
2022-01-12 17:05:02 +0300

    second

--- js/main.js
+++ js/main.js
@@ -1,37 +1,83 @@
 function myFunction() {
     // Get the value of the input field with id="numb"
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var pass1 = document.getElementById("pass1").value;
-    var pass2 = document.getElementById("pass2").value;
-    var age = document.getElementById("age").value;
-    var phone = document.getElementById("phone").value;
-    var url = document.getElementById("url").value;
-    var textarea = document.getElementById("textarea").value;
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let pass1 = document.getElementById("pass1").value;
+    let pass2 = document.getElementById("pass2").value;
+    let age = document.getElementById("age").value;
+    let phone = document.getElementById("phone").value;
+    let url = document.getElementById("url").value;
+    let textarea = document.getElementById("textarea").value;
 
-
+    let passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/; 
+    
     // If x is Not a Number or less than one or greater than 10
-    var name_error,emailerror;
-    if (name.length < 10 && name.length > 0) {
-        name_error = "your name is :" + name;
+ 
+    if ( name.length > 0&&name.length < 10) {
+        document.getElementById("name_error").innerHTML = "your name is :" + name;
+        alert(name.length);
+
     }
     else {
-        name_error = "Input not valid";
+        document.getElementById("name_error").innerHTML = "Input not valid";
 
     }
-    document.getElementById("name_error").innerHTML = name_error;
 
+
+    /***************   ****************/
+
+
+
+    if (email.indexOf("@"))
+     {
+        
+        document.getElementById("email-error").innerHTML = "Input ok";
+
+    }
+    else {
+        document.getElementById("email-error").innerHTML = "Input not valid";
+
+    }
+ /*********************** */
+
+    if ( passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/ )
+     {
+        
+        document.getElementById("pass1-error").innerHTML = "Input ok";
+
+    }
+    else {
+        document.getElementById("pass1-error").innerHTML = "Input not valid";
+
+    }
+ 
 
     
+    if ( age >25)
+     {
+        
+        document.getElementById("age-error").innerHTML = "Input ok";
 
-if (email.indexOf("@") > 0) {
-    emailerror = "invaild email";
-}
-else {
-    emailerror = "Input not valid";
+    }
+    else {
+        document.getElementById("age-error").innerHTML = "Input not valid";
 
-}
-document.getElementById("name_error").innerHTML = emailerror;
+    }
+
+
+    if (pass1=!pass2 )
+     {
+        
+        document.getElementById("pass2-error").innerHTML = "Input ok";
+
+    }
+    else {
+        document.getElementById("pass2-error").innerHTML = "Input not valid";
+
+    }
+ 
+  
+  
 
 
 }
